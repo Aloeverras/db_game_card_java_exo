2128e8ee8cfd677e347784c7acf5940c297eddb8
Aloeverras <edouard.bouche@gmail.com>
2025-09-18 12:14:18 +0000

    repise

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # README file : db_game_card_java_exo
+
+
+## Principals classes project
+- **BaseModel** : base class mother of all classes.
---
+++ game_card/src/main/java/com/game_card/backend_card/models/BaseModel.java
@@ -0,0 +1,25 @@
+package com.game_card.backend_card.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseModel<T> {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    protected Long id;
+    protected T nameModel;
+
+    public abstract Long getId();
+    public abstract T getNameModel();
+    public abstract String fullNameModel();
+}
